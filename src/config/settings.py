eff179fb65b8498825b92c7d02e3ff463e5c0241
9Sumrak6 <step@Ascorbinka.ascorbinka.org>
2025-03-05 23:58:55 +0300

    Fix

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,16 +10,6 @@ class Settings(BaseSettings):
     db_test_name: str = "your_test_db_name"
     max_connection_count: int = 10
 
-    def __init__(
-        self,
-        db_host="127.0.0.1:5432",
-        db_name="your_db",
-        db_username="your_username",
-        db_password="your_password",
-        db_test_name="your_test_db_name",
-        max_connection_count=10
-    )
-
     @property
     def database_url(self) -> str:
         return "postgresql+asyncpg://" + self.db_username + ":" + self.db_password + "@" + self.db_host + "/" + self.db_name
